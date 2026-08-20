f6e40e1850615ecf39ee433a8996901560d1a13e
root <root@orangepi4pro>
2026-08-20 13:09:09 +0000

    feat: add USB power bank charging mode and update boot script
    - Add charger_mode kernel parameter to power supply driver
      - powerbank: limit input current to 500mA (safe for USB power banks)
      - fast: allow up to 1500mA (wall adapter)
      - (empty): default 1000mA
    - Update boot.cmd to pass charger_mode and extraargs to kernel
    - Add commented charger_mode=powerbank to orangepiEnv.txt
      (uncomment when powering from USB power bank)

--- boot/boot.cmd
+++ boot/boot.cmd
@@ -6,8 +6,14 @@ env import -t ${loadaddr} ${filesize}
 # Get root partition UUID
 part uuid mmc 0:1 rootdev
 
-# Boot arguments
-setenv bootargs "console=ttyS0,115200 root=PARTUUID=${rootdev} rootwait rw rootfstype=ext4 loglevel=${verbosity} sunxi_fc=fdt,${fdtfile} cma=${cma}"
+# Build boot arguments
+setenv bootargs "console=ttyS0,115200 root=PARTUUID=${rootdev} rootwait rw rootfstype=ext4 loglevel=${verbosity} sunxi_fc=fdt,${fdtfile} cma=${cma} ${extraargs}"
+
+# Charger mode: set "charger_mode=powerbank" in orangepiEnv.txt
+# to limit USB input current to 500mA (safe for power banks)
+if test -n "${charger_mode}"; then
+	setenv bootargs "${bootargs} charger_mode=${charger_mode}"
+fi
 
 # Load boot files
 ext4load mmc 0:1 ${ramdisk_addr_r} /boot/uInitrd
